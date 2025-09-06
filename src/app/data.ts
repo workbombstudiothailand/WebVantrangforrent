56de0cc901bdac2567ac8fa1a202abb7b1bf326d
workbombstudiothailand <workbombstudio@outlook.com>
2025-09-06 11:55:52 +0700

    update web vantrangforrent 10 RC

---
+++ src/app/data.ts
@@ -0,0 +1,44 @@
+export const services = [
+    {
+        title: 'Toyota Hiace',
+        category: 'Hiace',
+        price: '2,500 บาท/วัน',
+        image: '/Hiace.jpg',
+        features: ['ภายในหรูหรา', 'นั่งสบาย', 'แอร์เย็น', 'รถใหม่']
+    },
+    {
+        title: 'Toyota Commuter All new',
+        category: 'Commuter',
+        price: '1,800 - 2,000 บาท/วัน',
+        image: '/commuterold.jpg',
+        features: ['ภายในหรูหรา', 'นั่งสบาย', 'แอร์เย็น', 'รถใหม่']
+    },
+    {
+        title: 'รถตู้หรู VIP Alphard',
+        category: 'vipAlphard',
+        price: '7,500 - 8,500 บาท/วัน',
+        image: '/alphardleft.jpg',
+        features: ['บริการดี', 'คนขับมืออาชีพ', 'รถสวย สะอาด นั่งสบาย มีระดับ', 'ใส่ใจลูกค้าตลอดการเดินทาง']
+    }
+];
+
+export const testimonials = [
+    {
+        image: '/thkone.jpg'
+    },
+    {
+        image: '/thktwo.jpg'
+    },
+    {
+        image: '/thkthree.jpg'
+    }
+];
+
+export const destinations = [
+    'ตรัง',
+    'กระบี่',
+    'พัทลุง',
+    'สตูล',
+    'สงขลา',
+    'นครศรีธรรมราช'
+];
