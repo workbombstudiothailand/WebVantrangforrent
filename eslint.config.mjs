0f56254de717541101e3ee3ec6642b872a2dd559
workbombstudiothailand <workbombstudio@outlook.com>
2025-09-06 13:23:15 +0700

    update web vantrangforrent 18 RC

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,21 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import nextPlugin from '@next/eslint-plugin-next';
+import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
 export default tseslint.config(
-    { ignores: ['dist'] },
-    {
-        files: ['**/*.{ts,tsx}'],
-        languageOptions: {
-            parser: tseslint.parser,
-            ecmaVersion: 2020,
-            globals: globals.browser,
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-            '@next/next': nextPlugin,
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            ...tseslint.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            ...nextPlugin.configs.recommended.rules,
-            ...nextPlugin.configs['core-web-vitals'].rules,
-            'react-refresh/only-export-components': [
-                'warn',
-                { allowConstantExport: true },
-            ],
-        },
-    }
+  {
+    ignores: ['dist', '.next'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
 );
