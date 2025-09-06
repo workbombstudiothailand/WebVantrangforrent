94dffd464728a0272b93b518a9894200a504fc32
workbombstudiothailand <workbombstudio@outlook.com>
2025-09-06 20:50:36 +0700

    update web vantrangforrent 26.2 RC

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,31 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'scontent.furt1-1.fna.fbcdn.net',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-        'http://localhost:3000',
-        'https://www.vantrangforrent.com',
-        'http://119.59.120.13',
-        'http://192.168.0.109:3000',
-        'http://192.168.0.116:3000',
-        'http://192.168.0.117:3000',
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'scontent.furt1-1.fna.fbcdn.net',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    devIndicators: {
+        allowedDevOrigins: [
+            'http://localhost:3000',
+            'https://www.vantrangforrent.com',
+            'http://119.59.120.13',
+            'http://192.168.0.109:3000',
+            'http://192.168.0.116:3000',
+            'http://192.168.0.117:3000',
+        ],
+    },
 };
 
 export default nextConfig;
